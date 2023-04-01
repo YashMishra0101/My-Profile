0e28e60eff22a67c50fc315b861df18719d85726
Yash Mishra <yashrkmfedu6927@gmail.com>
2023-04-01 13:55:05 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const bottom =document.querySelector(".bottom")
+
+const openbutton=()=>{
+  bottom.classList.add("active")
+}
+
+const closebutton=()=>{
+  bottom.classList.remove("active")
+}
+
+
+
